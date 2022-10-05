82ede38ae43bb050b5998afbfefc088940544a5d
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-10-05 17:56:50 +0200

    I did every exercise except the exercise 8

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+"use strict";
+/* Crea un proyecto con typescript en el que declares un array que almacene valores de
+            nombres de personas. Recorre en un bucle el array e imprímelo en el navegador a través
+            de una lista desordenada. */
+let personas = [
+    "Luis",
+    "Laura",
+    "Lorenzo"
+];
+document.write("<ul>");
+personas.forEach(persona => {
+    document.write(`<li>${persona}</li>`);
+});
+document.write("</ul>");
