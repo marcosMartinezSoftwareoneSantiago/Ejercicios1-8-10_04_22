e4debc0b4895f09e165799aaa1aec46e7b7209ea
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-10-06 12:05:23 +0200

    All exercises have being done

--- ejercicio2.js
+++ ejercicio2.js
@@ -2,13 +2,13 @@
 /* Crea un proyecto con typescript en el que declares un array que almacene valores de
             nombres de personas. Recorre en un bucle el array e imprímelo en el navegador a través
             de una lista desordenada. */
-let personas = [
+let nombrePersonas = [
     "Luis",
     "Laura",
     "Lorenzo"
 ];
 document.write("<ul>");
-personas.forEach(persona => {
+nombrePersonas.forEach(persona => {
     document.write(`<li>${persona}</li>`);
 });
 document.write("</ul>");
--- ejercicio2.ts
+++ ejercicio2.ts
@@ -2,13 +2,13 @@
             nombres de personas. Recorre en un bucle el array e imprímelo en el navegador a través
             de una lista desordenada. */
 
-let personas:string[] = [
+let nombrePersonas:string[] = [
     "Luis",
     "Laura",
     "Lorenzo"
 ];
 document.write("<ul>");
-personas.forEach(persona => {
+nombrePersonas.forEach(persona => {
     document.write(`<li>${persona}</li>`);
 });
 document.write("</ul>");
